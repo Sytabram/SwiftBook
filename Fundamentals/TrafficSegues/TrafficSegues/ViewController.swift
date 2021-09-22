3fda6841501e20044be1239e503d10693fad2f4f
Sytabram <bryan.zweiacker@gmail.com>
2021-09-22 16:54:17 +0200

    Add TrafficSegues

--- Fundamentals/TrafficSegues/TrafficSegues/ViewController.swift
+++ Fundamentals/TrafficSegues/TrafficSegues/ViewController.swift
@@ -9,12 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segueSwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
-     
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+        performSegue(withIdentifier: "Yellow", sender: nil)
+        }
     }
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+    
 }
 
